80a967caaee64b8e9a44f9d370b700011cd286e4
Joseph W Becher <jwbecher@drazisil.com>
2017-01-07 12:01:03 -0500

    bootstrap shell
    [ci skip]

--- server.js
+++ server.js
@@ -3,26 +3,25 @@ var express = require('express')
 var http = require('http')
 var bodyParser = require('body-parser')
 
-var buildBot = require('./src/index.js')
+var buildwatch = require('./src/index.js')
 
 var app = express()
 
 app.use(bodyParser.json()) // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
 
-app.set('port', 3000)
-
-app.get('/', function (req, res) {
-  res.send('Ok')
-})
+// Server port is set by PORT env or web_port from config file with fallback to 3000
+app.set('port', process.env.PORT || config.web_port || 3000)
 
 app.post('/hooks/circleci', function (req, res) {
   // console.dir(req.body)
-  buildBot.processPayload(req.body)
+  buildwatch.processPayload(req.body)
   res.send('Ok')
 })
 
-buildBot.init(config)
+app.use(express.static('public'))
+
+buildwatch.init(config)
 
 http.createServer(app).listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'))
