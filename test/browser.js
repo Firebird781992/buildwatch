8d976cdb62eb8d85ea9150b7611607330b606ce3
Joseph W Becher <jwbecher@drazisil.com>
2017-01-07 16:06:24 -0500

    Ah hem :)

--- test/browser.js
+++ test/browser.js
@@ -1,21 +1,26 @@
 var chromedriver = require('chromedriver')
+var geckodriver = require('geckodriver')
 
 var args = [
     // optional arguments
 ]
 
+function testGoogle (browser) {
+  chromedriver.start(args)
+  geckodriver.start(args)
+  browser
+    .url('http://www.google.com')
+    .waitForElementVisible('body', 1000)
+    .setValue('input[type=text]', 'nightwatch')
+    .waitForElementVisible('button[name=btnG]', 1000)
+    .click('button[name=btnG]')
+    .pause(2000)
+    .assert.containsText('#main', 'Night Watch')
+    .end()
+  chromedriver.stop()
+  geckodriver.stop()
+}
+
 module.exports = {
-  'Demo test Google': function (browser) {
-    chromedriver.start(args)
-    browser
-      .url('http://www.google.com')
-      .waitForElementVisible('body', 1000)
-      .setValue('input[type=text]', 'nightwatch')
-      .waitForElementVisible('button[name=btnG]', 1000)
-      .click('button[name=btnG]')
-      .pause(1000)
-      .assert.containsText('#main', 'Night Watch')
-      .end()
-    chromedriver.stop()
-  }
+  'Demo test Google': testGoogle
 }
